4801e71259522134c21ce369aed846493fd25ba9
= <=>
2026-01-01 20:46:14 -0300

    kernel extend

--- mllse/configuration.nix
+++ mllse/configuration.nix
@@ -2,7 +2,16 @@
 
 {
   # PIN THE KERNEL (choose one that yt6801 exists for)
-  boot.kernelPackages = pkgs.linuxPackages_6_6;
+  # boot.kernelPackages = pkgs.linuxPackages_6_6;
+
+  # 1. Extend the kernel packages to include the out-of-tree module
+  boot.kernelPackages = pkgs.linuxPackages_6_6.extend (self: super: {
+    yt6801 = super.yt6801.overrideAttrs (oldAttrs: {
+      # This ensures the module isn't marked as 'broken' or skipped
+      meta = oldAttrs.meta // { broken = false; };
+    });
+    
+  });
 
   isoImage.storeContents = [
     config.boot.kernelPackages.yt6801
