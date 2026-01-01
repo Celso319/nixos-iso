c728b6526f5c6d36d7ba37043a5b59042443798f
= <=>
2026-01-01 20:52:59 -0300

    remove kernel packages define

--- mllse/configuration.nix
+++ mllse/configuration.nix
@@ -1,8 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # PIN THE KERNEL (choose one that yt6801 exists for)
-  boot.kernelPackages = pkgs.linuxPackages_6_6;
 
   # SHIP THE MODULE INTO THE ISO
   boot.extraModulePackages = [
