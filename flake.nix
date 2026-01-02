f791190b3e28c47c5629442f53f9a13542c45e1c
= <=>
2026-01-01 21:31:38 -0300

    fix nixpkgs url

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Celso NixOS ISO builds";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/tree/nixos-25.11";
   };
 
   outputs = { self, nixpkgs }:
